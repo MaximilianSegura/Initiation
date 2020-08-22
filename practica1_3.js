7ce3ad0379654ab3b14939b1689af148c74dbe36
Miguel Frias <22214@oaklawnacademy.org>
2020-08-22 12:21:24 -0500

    Ajuste a prac 1

--- practica1_3.js
+++ practica1_3.js
@@ -7,10 +7,11 @@ for(var i=1;i<=10;i++){
 function DNA_strand(strand){
 	var solution = null; //no te preocupes por esto, solo lo estoy declarando
 
-	
+
 	/*
 	Aqui va a ir todo tu codigo. El input es la variable strand, no te preocupes, ya esta declarada. 
 	Tu respuesta deberá de estar en la variable solution :)
+	No toques la parte de return ( y tu codigo no debe estar debajo de return)
 	*/
 
 	return solution
